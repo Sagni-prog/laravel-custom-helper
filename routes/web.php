a19c6db2e77fc31a25d752f72bbeac26f12212a3
Sagni Alemayehu <sagnialemayehu69@gmail.com>
2023-05-06 00:06:58 +0300

    ###using custom helper in view###

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('hours');
 });
 
 Route::get('/hours',function() {
    
-   return seconds_to_hours(360);
+   return seconds_to_hours(1000);
 });
